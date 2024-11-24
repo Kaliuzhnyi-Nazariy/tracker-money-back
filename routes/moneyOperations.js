e3c0aeaf67ee9a56e04d649de82b7ca6da168786
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2024-11-24 16:48:59 +0200

    routes

---
+++ routes/moneyOperations.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const { authenticate, validationBody } = require("../middlewares/");
+const ctr = require("../controllers/expensesOperations");
+const ctrl = require("../controllers/earningsOperations");
+const { makeOperationsValidation } = require("../models/moneySchema");
+
+const router = express.Router();
+
+router.get("/allExpenses", authenticate, ctr.getAllExpenses);
+
+router.post(
+  "/newExpenses",
+  authenticate,
+  validationBody(makeOperationsValidation),
+  ctr.addExpenses
+);
+
+router.put(
+  "/expenses/:expensesId",
+  authenticate,
+  validationBody(makeOperationsValidation),
+  ctr.updateExpenses
+);
+
+router.delete("/expenses/remove/:expensesId", authenticate, ctr.removeExpenses);
+
+router.get("/allEarnings", authenticate, ctrl.getAllEarnings);
+
+router.post(
+  "/newEarnings",
+  authenticate,
+  validationBody(makeOperationsValidation),
+  ctrl.addEarnings
+);
+
+router.put(
+  "/earnings/:earningsId",
+  authenticate,
+  validationBody(makeOperationsValidation),
+  ctrl.updateEarnings
+);
+
+router.delete(
+  "/earnings/remove/:earningsId",
+  authenticate,
+  ctrl.removeEarnings
+);
+
+module.exports = router;
---
+++ routes/user.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const ctr = require("../controllers/user");
+const { authenticate, validationBody } = require("../middlewares/");
+const { schemas } = require("../models/userSchema");
+const router = express.Router();
+
+router.post("/register", validationBody(schemas.regValidation), ctr.register);
+
+router.post("/login", validationBody(schemas.loginValidation), ctr.login);
+
+router.post("/logout", authenticate, ctr.logout);
+
+module.exports = router;
