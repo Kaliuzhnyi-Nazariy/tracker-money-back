2f034353139f9b74d76875046c861223c19a3c33
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2024-11-24 16:48:21 +0200

    money operations controllers

---
+++ controllers/earningsOperations.js
@@ -0,0 +1,73 @@
+const { ErrorHelper, ctrlWrapper } = require("../helpers");
+const { Money } = require("../models/moneySchema");
+
+const getAllEarnings = async (req, res, next) => {
+  const { id } = req.user;
+
+  const earnings = await Money.find({ owner: id, type: "earnings" });
+
+  if (!earnings) next(ErrorHelper(404));
+
+  res.status(200).json(earnings);
+};
+
+const addEarnings = async (req, res, next) => {
+  const { id: owner } = req.user;
+  const { title, date, price, type } = req.body;
+
+  const newEarnings = {
+    title,
+    date,
+    price,
+
+    owner,
+    type,
+  };
+
+  await Money.create(newEarnings);
+
+  res.status(201).json(newEarnings);
+};
+
+const updateEarnings = async (req, res, next) => {
+  const { id: owner } = req.user;
+  const { earningsId } = req.params;
+  const { title, date, price } = req.body;
+
+  const isEarnings = await Money.findOne({ owner, _id: earningsId });
+
+  if (!isEarnings) next(ErrorHelper(404));
+
+  const newUpdatedEarnings = {
+    title,
+    date,
+    price,
+
+    owner,
+    type: isEarnings.type,
+  };
+
+  await Money.findByIdAndUpdate(earningsId, newUpdatedEarnings);
+
+  res.status(200).json(newUpdatedEarnings);
+};
+
+const removeEarnings = async (req, res, next) => {
+  const { id: owner } = req.user;
+  const { earningsId } = req.params;
+
+  const isEarnings = await Money.findOne({ owner, _id: earningsId });
+
+  if (!isEarnings) next(ErrorHelper(404));
+
+  await Money.findByIdAndDelete(earningsId);
+
+  res.status(200).json(isEarnings);
+};
+
+module.exports = {
+  getAllEarnings: ctrlWrapper(getAllEarnings),
+  addEarnings: ctrlWrapper(addEarnings),
+  updateEarnings: ctrlWrapper(updateEarnings),
+  removeEarnings: ctrlWrapper(removeEarnings),
+};
---
+++ controllers/expensesOperations.js
@@ -0,0 +1,73 @@
+const { ErrorHelper, ctrlWrapper } = require("../helpers");
+const { Money } = require("../models/moneySchema");
+
+const getAllExpenses = async (req, res, next) => {
+  const { id } = req.user;
+
+  const expenses = await Money.find({ owner: id, type: "expenses" });
+
+  if (!expenses) next(ErrorHelper(404));
+
+  res.status(200).json(expenses);
+};
+
+const addExpenses = async (req, res, next) => {
+  const { id: owner } = req.user;
+  const { title, date, price, category, type } = req.body;
+
+  const newExpenses = {
+    title,
+    date,
+    price,
+    category,
+    owner,
+    type,
+  };
+
+  await Money.create(newExpenses);
+
+  res.status(201).json(newExpenses);
+};
+
+const updateExpenses = async (req, res, next) => {
+  const { id: owner } = req.user;
+  const { expensesId } = req.params;
+  const { title, date, price, category } = req.body;
+
+  const isExpense = await Money.findOne({ owner, _id: expensesId });
+
+  if (!isExpense) next(ErrorHelper(404));
+
+  const newUpdatedExpenses = {
+    title,
+    date,
+    price,
+    category,
+    owner,
+    type: isExpense.type,
+  };
+
+  await Money.findByIdAndUpdate(expensesId, newUpdatedExpenses);
+
+  res.status(200).json(newUpdatedExpenses);
+};
+
+const removeExpenses = async (req, res, next) => {
+  const { id: owner } = req.user;
+  const { expensesId } = req.params;
+
+  const isExpense = await Money.findOne({ owner, _id: expensesId });
+
+  if (!isExpense) next(ErrorHelper(404));
+
+  await Money.findByIdAndDelete(expensesId);
+
+  res.status(200).json(isExpense);
+};
+
+module.exports = {
+  getAllExpenses: ctrlWrapper(getAllExpenses),
+  addExpenses: ctrlWrapper(addExpenses),
+  updateExpenses: ctrlWrapper(updateExpenses),
+  removeExpenses: ctrlWrapper(removeExpenses),
+};
